090188b30c4e11d4eb56c18cbe60134240896435
junhee916 <junhee916@naver.com>
2021-04-26 11:45:37 +0900

    bcrypt.compare login/jwt get token

--- router/user.js
+++ router/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const userModel = require('../model/user')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 
 // userInfo
@@ -91,6 +92,44 @@ router.post('/signup', (req, res) => {
 // 로그인
 router.post("/login", (req, res) => {
 
+    // email 유무 check => password 매칭 => 로그인 성공시 token 반환
+
+    userModel
+        .findOne({email : req.body.userEmail})
+        .then(user => {
+            if(!user){
+                return res.status(404).json({
+                    msg : "user email, please other email"
+                })
+            }
+            else{
+                bcrypt.compare(req.body.password, user.password, (err, isMatch) =>{
+                    if(err || isMatch === false){
+                        return res.status(408).json({
+                            msg : "password not match"
+                        })
+                    }
+                    else{
+                        const token = jwt.sign(
+                            {id : user._id, email : user.email},
+                            process.env.SECRET_KEY,
+                            { expiresIn: "1h"}
+                        )
+
+                        res.json({
+                            msg : "successful login",
+                            userInfo : user,
+                            tokenInfo : token
+                        })
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                msg : err.message
+            })
+        })
 })
 
 module.exports = router
